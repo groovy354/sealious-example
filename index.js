e54dca999d236e421a0888fcb22cdc69ac157eb3
modliszka <nikapia@wp.pl>
2015-09-09 13:40:28 +0200

    Add require sealious-www-server plugin

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+require("sealious-www-server");
 var Sealious = require("sealious");
-//var Promise = require("bluebird");
-
-
 Sealious.init();
 
+
+//console.log("robię loga  ", Sealious.ChipManager.get_chip_amount_by_type("channel")); 
+
 var www_server = Sealious.ChipManager.get_chip("channel", "www_server");
-www_server.static_route("E:\\Projekt Sealious\\sealious-example\\www_server_files");
+//console.log("po get_chip ",www_server);
+
+//www_server = WwwServer.static_route("E:\\Projekt Sealious\\sealious-example\\www_server_files");
 
 var someone = new Sealious.ChipTypes.ResourceType({
 	name: "person",
